a65a466613d13f7d430688ca84cf55e8fa3b072c
0xdipak <dipaksharma34447255@gmail.com>
2024-12-08 18:43:17 +0545

    code updated

---
+++ src/components/RuLetter.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+
+const RuLetter = () => {
+ 
+
+  return (
+    <>
+      <div className="w-full h-[500px] flex items-center justify-center perspective-1000 cursor-move">
+        <div
+          className="text-9xl md:text-[200px] font-bold transition-transform duration-100 ease-out"
+          style={{
+            transformStyle: "preserve-3d",
+            backgroundImage: "linear-gradient(45deg, #DC143C 0%, #003893 100%)",
+            WebkitBackgroundClip: "text",
+            color: "transparent",
+            backgroundClip: "text",
+          }}
+        >
+          रु
+        </div>
+        <div
+          className="text-9xl md:text-[200px] font-bold"
+          style={{
+            transformStyle: "preserve-3d",
+            backgroundImage: "linear-gradient(45deg, #DC143C 0%, #003893 100%)",
+            WebkitBackgroundClip: "text",
+            color: "transparent",
+            backgroundClip: "text",
+          }}
+        >
+          Pay
+        </div>
+      </div>
+      <div
+        className="max-w-7xl mx-auto text-xl font-bold text-right -mt-28"
+        style={{
+          transformStyle: "preserve-3d",
+          backgroundImage: "linear-gradient(45deg, #DC143C 0%, #003893 100%)",
+          WebkitBackgroundClip: "text",
+          color: "transparent",
+          backgroundClip: "text",
+        }}
+      >
+        —coming soon!
+      </div>
+    </>
+  );
+};
+
+export default RuLetter;
